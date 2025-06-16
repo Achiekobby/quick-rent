9d5e2041491fb22fcde0f10b969fb7c96c336b59
Achiekobby <achiekobby17@gmail.com>
2025-06-16 16:44:09 +0000

    config file update

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -1,4 +1,5 @@
-const baseUrl = "http://84.247.181.60:2025/api";
+// const baseUrl = "http://84.247.181.60:2025/api";
+const baseUrl = "https://gba.quicksmscontent.com/api";
 
 const Config = {
   baseUrl: baseUrl,
