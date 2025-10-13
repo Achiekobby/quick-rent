969c6955f6893d595e7f34edf0274652f982104d
Achiekobby <achiekobby17@gmail.com>
2025-10-13 14:00:48 +0000

    backend_url change implemented

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -1,5 +1,6 @@
 // const baseUrl = "http://84.247.181.60:2025/api";
-const baseUrl = "https://gba.quicksmscontent.com/api";
+// const baseUrl = "https://gba.quicksmscontent.com/api";
+const baseUrl = "https://rent.quicksmscontent.com/api";
 
 const Config = {
   baseUrl: baseUrl,
